68766306861c6ce55641ed098e20a794a9c97350
Edwin Gerard <edwin.gerard1@gmail.com>
2018-05-22 18:02:45 +0200

    Mise en forme des formulaires

--- src/AppBundle/Form/RegistrationType.php
+++ src/AppBundle/Form/RegistrationType.php
@@ -20,9 +20,9 @@ class RegistrationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName', TextType::class, array('label' => 'form.firstName', 'translation_domain' => 'FOSUserBundle'))
-            ->add('lastName', TextType::class, array('label' => 'form.lastName', 'translation_domain' => 'FOSUserBundle'))
-            ->add('location', TextType::class, array('label' => 'form.location', 'translation_domain' => 'FOSUserBundle'));
+            ->add('firstName', TextType::class, array('label' => 'Prénom', 'translation_domain' => 'FOSUserBundle'))
+            ->add('lastName', TextType::class, array('label' => 'Nom', 'translation_domain' => 'FOSUserBundle'))
+            ->add('location', TextType::class, array('label' => 'Ville', 'translation_domain' => 'FOSUserBundle'));
     }
 
     public function getParent()
