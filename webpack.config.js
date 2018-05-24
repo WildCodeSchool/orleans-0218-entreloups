5f12c3e2846a65bb2297d025fcd2d4b79eecbd37
Gauthier Arnaud <Vilc86@hotmail.com>
2018-05-24 09:48:53 +0200

    Navbar page d'accueill terminé

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 let Encore = require('@symfony/webpack-encore');
 
 Encore
-    .setOutputPath('web/build/')
-    .setPublicPath('/web')
+    .setOutputPath('web/build')
+    .setPublicPath('/build')
     .addEntry('app', './assets/js/app.js')
     .cleanupOutputBeforeBuild()
     .enableSassLoader()
-    .addEntry('style', './assets/scss/main.scss')
-    .enableBuildNotifications();
+    .addStyleEntry('style', './assets/scss/main.scss')
+    .enableBuildNotifications()
+;
 
 module.exports = Encore.getWebpackConfig();
