0c8af98f9c57ae82e8bfb4eef55d81a5edc156f7
Edwin Gerard <edwin.gerard1@gmail.com>
2018-05-29 16:29:43 +0200

    Surcharge du formulaire de FOS

--- src/AppBundle/Form/ProfileType.php
+++ src/AppBundle/Form/ProfileType.php
@@ -9,7 +9,31 @@
 namespace AppBundle\Form;
 
 
-class ProfileType
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ProfileType extends AbstractType
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName', TextType::class, array('label' => 'Prénom', 'translation_domain' => 'FOSUserBundle'))
+            ->add('lastName', TextType::class, array('label' => 'Nom', 'translation_domain' => 'FOSUserBundle'))
+            ->add('location', TextType::class, array('label' => 'Ville', 'translation_domain' => 'FOSUserBundle'));
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\ProfileFormType';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_profile';
+    }
 
 }
