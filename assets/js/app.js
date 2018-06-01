3ff0fc35b13d0a14ef640c479255b2a130967a46
Xavier Meckler <xmeckler@gmail.com>
2018-06-01 10:30:43 +0200

    Transfert du JS/jQuery pour le message d'alerte de suppression dans le fichier app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,9 @@
 import $ from "jquery"
 
 import 'bootstrap/dist/js/bootstrap'
+
+let deleteButton = document.getElementsByClassName('confirm-alert');
+
+$(deleteButton).click(function () {
+    return confirm ("Etes-vous sûr(e) de vouloir supprimer cet élément ?");
+});
