2587f2318f1fbcec3c7b4a92f709bc403b5c77c7
Xavier Meckler <xmeckler@gmail.com>
2018-06-01 11:59:38 +0200

    merge

---
+++ src/AppBundle/DataFixtures/EventFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\DataFixtures;
+
+use AppBundle\Entity\Event;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+class EventFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+        // create 20 event !
+        for ($i = 0; $i < 20; $i++) {
+            $event = new Event();
+            $event->setTitle(ucfirst($faker->words(3, true)));
+            $event->setCity($faker->city);
+            $event->setDescription($faker->text);
+            $event->setImage('image.jpg');
+            $manager->persist($event);
+        }
+
+        $manager->flush();
+    }
+}
