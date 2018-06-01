9e4a7f00c17c8a313fa0ad63f00360929e2ed61f
Xavier Meckler <xmeckler@gmail.com>
2018-06-01 11:43:18 +0200

    Correction de la fonctionalité d'alert on delete pour être entièrement en jQuery

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,6 @@ import $ from "jquery"
 
 import 'bootstrap/dist/js/bootstrap'
 
-let deleteButton = document.getElementsByClassName('confirm-alert');
-
-$(deleteButton).click(function () {
+$('.confirm-alert').click(function () {
     return confirm ("Etes-vous sûr(e) de vouloir supprimer cet élément ?");
 });
