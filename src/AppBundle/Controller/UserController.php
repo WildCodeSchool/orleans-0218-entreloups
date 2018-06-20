725c099f647e008c7b4842001b8d2a51e4192938
Damien SEDIRA <es.todo78@yahoo.fr>
2018-06-20 14:59:21 +0200

    Création d'une page pour rechercher un utilisateur avec son email

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder21
+ * Date: 19/06/18
+ * Time: 17:40
+ */
+
+namespace AppBundle\Controller;
+use AppBundle\Entity\User;
+use AppBundle\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * User controller.
+ *
+ * @Route("user")
+ */
+class UserController extends Controller
+{
+    /**
+     * Displays a search field to find one particular user.
+     *
+     * @Route("/search/edition/{id}", name="search_user")
+     * @Method({"GET", "POST"})
+     */
+    public function searchAction(Request $request)
+    {
+        $form = $this->createForm(UserType::class);
+        $form->handleRequest(($request));
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $user = $form->getData();
+            $email = $user->getEmail();
+            $searchedUser = null;
+            $searchedUser = $em->getRepository(User::class)->findOneByEmail($email);
+
+            return $this->render('user/searchUser.html.twig', array(
+                'form' => $form->createView(),
+                'searchedUser' => $searchedUser,
+            ));
+        }
+
+        return $this->render('user/searchUser.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+}
